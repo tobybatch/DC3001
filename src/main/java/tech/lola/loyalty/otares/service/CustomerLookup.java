1e5136c19644502da23377648ec4f10db70aa7b5
Toby Batch <tobias@neontribe.co.uk>
2019-10-16 08:41:53 +0100

    fixed indenting

--- src/main/java/tech/lola/loyalty/otares/service/CustomerLookup.java
+++ src/main/java/tech/lola/loyalty/otares/service/CustomerLookup.java
@@ -22,31 +22,31 @@ public class CustomerLookup {
 
 	@Value("${otares.api.endpoint}")
 	private String apiRoute;
-			
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerLookup.class);
 
 	public CustomerDto getCustomer(String id) {
 
 		// String customerId = "29533087";
 		String customerId = "1";
-		
+
 		// We have the customer id, get the customer
 		String oatresCustomerEndoint = this.apiRoute + "/customers/" + customerId;
 		LOGGER.info("oatresCustomerEndoint: " + oatresCustomerEndoint);
-		
+
 		// TODO catch 404
-        RestTemplate restTemplate = new RestTemplate();
-        CustomerDto customer = restTemplate.getForObject(oatresCustomerEndoint, CustomerDto.class);
-        LOGGER.info(customer.toString());
-        
-        // If this customer doesn't have a loyalty it's not in the program.
-        String loyaltyId = "010101010";
-        
-        // Does the use exist in Tally
-        
-        // Update it
-        // Create it
-        
-        return null;
+		RestTemplate restTemplate = new RestTemplate();
+		CustomerDto customer = restTemplate.getForObject(oatresCustomerEndoint, CustomerDto.class);
+		LOGGER.info(customer.toString());
+
+		// If this customer doesn't have a loyalty it's not in the program.
+		String loyaltyId = "010101010";
+
+		// Does the use exist in Tally
+
+		// Update it
+		// Create it
+
+		return null;
 	}
 }
